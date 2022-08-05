4762c0896cdd8d41eed9b7d4bdf860e860bae96e
MomboteQ <momboteq@gmail.com>
2022-08-05 16:51:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+
+### Desktop view
+![](screenshots/desktop1.png)
+![](screenshots/desktop1.png)
+
+### Mobile view
+![](screenshots/mobile1.png)
+![](screenshots/mobile2.png)
+
+### Links
+
+- Live Site URL: [Live Site](https://momboteq.github.io/interactive-rating-component)
+
+## My process
+
+### Built with
+
+- Flexbox
+- Desktop-first workflow
+- [Vue](https://vuejs.org) - JS framework
+
+## Author
+
+- Website - [MomboteQ.github.io](https://MomboteQ.github.io)
+- Frontend Mentor - [@MomboteQ](https://www.frontendmentor.io/profile/MomboteQ)
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import '@/assets/css/style.css'
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="rating" v-show="state == 'rating'">
+            <img src="@/assets/img/icon-star.svg" alt="star">
+            <h1>How did we do?</h1>
+            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+            <div class="rating__scale">
+                <button @click="rating = 1" :class="{ 'active': rating == 1 }">1</button>
+                <button @click="rating = 2" :class="{ 'active': rating == 2 }">2</button>
+                <button @click="rating = 3" :class="{ 'active': rating == 3 }">3</button>
+                <button @click="rating = 4" :class="{ 'active': rating == 4 }">4</button>
+                <button @click="rating = 5" :class="{ 'active': rating == 5 }">5</button>
+            </div>
+            <button class="rating__submit" @click="ratingSubmit()">Submit</button>
+        </div>
+        <Transition>
+            <div class="thank-you" v-show="state == 'thank you'">
+                <img src="@/assets/img/illustration-thank-you.svg" alt="phone">
+                <span>You selected {{ rating }} out of 5</span>
+                <h1>Thank you!</h1>
+                <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+            </div>
+        </Transition>
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://momboteq.github.io">MomboteQ</a>.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            state: 'rating',
+            rating: 0
+        }
+    },
+    methods: {
+        ratingSubmit() {
+            if (this.rating > 0) {
+                this.state = 'thank you'
+            }
+        }
+    }
+}
+</script>
